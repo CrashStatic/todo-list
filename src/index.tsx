966d25468131309cc433388e0c9c133fc5bf273d
CrashStatic <thecrashstatic@gmail.com>
2024-10-13 20:01:38 +0300

    Реализует поведение ссылок в header

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,22 @@ import './assets/scss/style.scss'
 import { ToDoListPage } from './pages/ToDoListPage';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { Header } from './components/Header/Header';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ToDoListPage />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/todo' element={<ToDoListPage />}></Route>
+      </Routes>
+    </BrowserRouter>
     <ToastContainer
       position="bottom-right"
       autoClose={5000}
