523db6056dddb220fe7f20b42c62ed7a1892333a
CrashStatic <thecrashstatic@gmail.com>
2024-10-14 14:43:28 +0300

    Настраивает hemlet для head и стилизует страницу 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom"
+import classes from './404.module.scss';
 
 export const NotFound = () => {
     return (
         <div className="container">
             <h1>Not Found</h1>
-            <Link to="/">To home</Link>
+            <div>
+                <p className={classes.description}>Осавь надежу всяк сюда входящий... И вернись назад, путник!</p>
+            </div>
+            <Link to="/" className={classes.link}>To home</Link>
         </div>
     )
 }
